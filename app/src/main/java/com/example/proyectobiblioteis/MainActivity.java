8c4f87b59b13135000108ffd08dc2a24d9ae08fc
iagorv <Iagorive18@gmail.com>
2025-02-10 09:49:45 +0100

    primer commit

---
+++ app/src/main/java/com/example/proyectobiblioteis/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.proyectobiblioteis;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.proyectobiblioteis.API.models.Book;
+import com.example.proyectobiblioteis.API.repository.BookRepository;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText contraseña;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+
+
+
+    }
+}
